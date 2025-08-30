65a614e06a5c19a11431e8c3d2492bff3079650b
Mahesh <maheshbabugarnepudi21@gmail.com>
2025-08-30 22:59:18 +0530

    Initial commit

--- src/main/java/com/example/EmsApplication.java
+++ src/main/java/com/example/EmsApplication.java
@@ -14,9 +14,22 @@ import java.util.List;
 @SpringBootApplication
 public class EmsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EmsApplication.class, args);
-//		WebController controller = container.getBean(WebController.class);
-//		controller.getAllEmployees();
+		ApplicationContext container = SpringApplication.run(EmsApplication.class, args);
+////		WebController controller = container.getBean(WebController.class);
+////		controller.getAllEmployees();
+//		DepartmentDAO d = container.getBean(DepartmentDAO.class);
+//		Department dept = container.getBean(Department.class);
+//		Department dept1 = container.getBean(Department.class);
+//		Department dept2 = container.getBean(Department.class);
+//		Department dept3 = container.getBean(Department.class);
+//		dept.setName("Development");
+//		dept1.setName("HR");
+//		dept2.setName("Design");
+//		dept3.setName("Accounts");
+//		d.saveDepartment(dept);
+//		d.saveDepartment(dept1);
+//		d.saveDepartment(dept2);
+//		d.saveDepartment(dept3);
 
 
 
